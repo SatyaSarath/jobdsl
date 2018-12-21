773ef98d75779bd38024fa7fd244310bd9e275f0
SatyaSarath <32695340+SatyaSarath@users.noreply.github.com>
2018-12-21 15:16:22 +0530

    1st push

---
+++ nodejsdocdsl.groovy
@@ -0,0 +1,25 @@
+job('nodejobdocdsl'){
+	scm{
+		git('https://github.com/SatyaSarath/docker-demo.git'){ node -> // is hudson.plugins.git.GitSCM
+			node / gitConfigName('satyasarath')
+			node / gitConfigEmail('satya.kotha@wipro.com')
+		}
+	}
+	triggers{
+		scm('H/5 * * * *')
+	}
+	wrappers{
+		nodejs('nodejs')
+	}
+	steps{
+		dockerBuildAndPublish {
+            repositoryName('satyasarath/nodejsjob1')
+            tag('${BUILD_NUMBER}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+		)
+		
+	}
+}
